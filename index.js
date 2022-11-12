589edc386d2e95795984b37faad5e3f8ce344b25
cblawe <cblawe@gmail.com>
2022-11-12 13:49:23 -0500

    Commit initial files

---
+++ index.js
@@ -0,0 +1,40 @@
+//get element by id for home-score and guest-score
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+//initialize homeCount
+let homeCount = 0
+//create homeScore functions for +1 +2 +3
+function home1() {
+    homeCount += 1
+    homeScore.textContent = homeCount
+ }
+
+function home2() {
+    homeCount += 2
+    homeScore.textContent = homeCount
+}
+
+function home3() {
+    homeCount += 3
+    homeScore.textContent = homeCount
+
+}
+
+//initialize guestCount
+let guestCount = 0
+//create guestScore functions for +1 +2 +3
+function guest1() {
+    guestCount += 1
+    guestScore.textContent = guestCount
+}
+
+function guest2() {
+    guestCount += 2
+    guestScore.textContent = guestCount
+}
+
+function guest3() {
+    guestCount += 3
+    guestScore.textContent = guestCount
+}
